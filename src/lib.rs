d0abf16201dff3366f75c8ef27009e205ed467d9
saxxie-dev <c@saxxie.dev>
2023-04-20 14:59:09 -0700

    Write first few basic tests for TDDing. only deal with first 2 ip addresses

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,18 @@ use std::net::IpAddr;
 
 pub trait IPStore {
     fn request_handled(&mut self, ip_address: IpAddr) -> ();
-    fn top100(&self) -> &[IpAddr; 100];
+    fn top100(&self) -> [Option<IpAddr>; 100];
     fn clear(&mut self) -> ();
 }
 
-pub struct PulleyIPStore<'a> {
+struct PulleyIPStore<'a> {
     request_counts: HashMap<IpAddr, &'a u32>,
     top100_set: HashSet<IpAddr>,
     top100_list: [Option<(IpAddr, &'a u32)>; 100],
 }
 
 impl<'a> PulleyIPStore<'a> {
-    fn new() -> PulleyIPStore<'a> {
+    pub fn new() -> PulleyIPStore<'a> {
         PulleyIPStore {
             request_counts: HashMap::new(),
             top100_set: HashSet::new(),
@@ -24,18 +24,107 @@ impl<'a> PulleyIPStore<'a> {
     }
 }
 
+impl<'a> IPStore for PulleyIPStore<'a> {
+    fn request_handled(&mut self, ip_address: IpAddr) -> () {
+        todo!()
+    }
+
+    fn top100(&self) -> [Option<IpAddr>; 100] {
+        todo!()
+    }
+
+    fn clear(&mut self) -> () {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::collections::{HashMap, HashSet};
+    use rand::{thread_rng, Rng};
+    use std::{
+        collections::{HashMap, HashSet},
+        net::{IpAddr, Ipv4Addr},
+    };
 
-    use crate::PulleyIPStore;
+    use crate::{IPStore, PulleyIPStore};
+
+    // Helper function - generate random IPv4 address
+    fn random_ip() -> IpAddr {
+        let mut rng = thread_rng();
+        IpAddr::V4(Ipv4Addr::new(
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+        ))
+    }
 
     #[test]
-    fn it_works() {
-        let mut j = 0;
-        for _i in 1..20000000 {
-            j += 1;
-        }
-        assert_eq!(j, 4);
+    fn it_handles_a_single_request() {
+        let mut ipStore = PulleyIPStore::new();
+        let myIp: IpAddr = random_ip();
+        ipStore.request_handled(myIp);
+        assert_eq!(
+            ipStore.top100()[0],
+            Some(myIp),
+            "First element should be the ip address which was added"
+        );
+        assert_eq!(ipStore.top100()[1], None, "Second element should be None");
+    }
+
+    #[test]
+    fn it_successfully_clears() {
+        let mut ipStore = PulleyIPStore::new();
+        let myIp: IpAddr = random_ip();
+        ipStore.request_handled(myIp);
+        ipStore.clear();
+        assert_eq!(
+            ipStore.top100()[0],
+            None,
+            "First element should be None after clearing"
+        );
+    }
+
+    #[test]
+    fn it_handles_multiple_request() {
+        let mut ipStore = PulleyIPStore::new();
+        let ip1: IpAddr = random_ip();
+        let ip2: IpAddr = random_ip();
+        ipStore.request_handled(ip1);
+        ipStore.request_handled(ip2);
+
+        assert_eq!(
+            ipStore.top100()[0],
+            Some(ip1),
+            "First element should be the first ip address which was added"
+        );
+        assert_eq!(
+            ipStore.top100()[1],
+            Some(ip2),
+            "Second element should be second ip address which was added"
+        );
+        assert_eq!(ipStore.top100()[1], None, "Third element should be None");
+    }
+
+    #[test]
+    fn it_handles_reordering_after_multiple_request() {
+        let mut ipStore = PulleyIPStore::new();
+        let ip1: IpAddr = random_ip();
+        let ip2: IpAddr = random_ip();
+        ipStore.request_handled(ip1);
+        ipStore.request_handled(ip2);
+        ipStore.request_handled(ip2);
+
+        assert_eq!(
+            ipStore.top100()[0],
+            Some(ip2),
+            "First element should be the second ip address, which was handled twice"
+        );
+        assert_eq!(
+            ipStore.top100()[1],
+            Some(ip1),
+            "Second element should be first ip address, which was handled once"
+        );
+        assert_eq!(ipStore.top100()[1], None, "Third element should be None");
     }
 }
